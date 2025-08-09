cfd0d294b6ed3d29791a630de874a0b1587a8536
Sachin Akhani <AkSachin@users.noreply.github.com>
2025-08-09 23:31:57 +0530

    Update main.js

--- src/content/getusermedia/resolution/js/main.js
+++ src/content/getusermedia/resolution/js/main.js
@@ -105,7 +105,7 @@ const fullHdConstraints = {
   video: {
     width: { exact: 1920 },
     height: { exact: 1080 },
-    frameRate: { exact: 60 }, // Force 60fps
+    frameRate: { ideal: 60, min: 60 }, // force 60 fps
     facingMode: "user"
   }
 };
